acd19226a98ae6446534a321ba2f95e4f59ffbe6
cdchrisDev <juan.feli.lopez@gmail.com>
2024-01-28 10:56:10 -0800

    conditions fixed, else if deleted

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,11 +6,11 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!node->right && !node->left)
-		return (1);
-	
 	if (!node)
 		return (0);
 
+	if (!node->right && !node->left)
+		return (1);
+	
 	return (0);
 }
