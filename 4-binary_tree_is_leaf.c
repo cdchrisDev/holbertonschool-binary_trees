86f147bff76955eb08fb247f5eb58431eda306a4
cdchrisDev <juan.feli.lopez@gmail.com>
2024-01-28 09:49:45 -0800

    just 2 conditions

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,6 +8,8 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (!node->right && !node->left)
 		return (1);
+	else if (node->right == NULL && node->left)
+		return (0);
 	else
 		return (0);
 }
